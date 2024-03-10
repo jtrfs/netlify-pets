07800b761615ae7d86c329e282c62d52fbc90892
Jan Varmuza <jan.varmuza@gmail.com>
2024-03-10 13:12:04 +0100

    Finished reading from database

--- netlify/functions/pets.js
+++ netlify/functions/pets.js
@@ -1,8 +1,20 @@
+const {MongoClient} = require('mongodb');
+
 const handler = async () => {
+  const client = new MongoClient(process.env.CONNECTIONSTRING);
+
+  await client.connect();
+  console.log('MongoDB connected');
+  const pets = await client.db().collection('pets').find().toArray();
+  await client.close();
+
   return {
     statusCode: 200,
-    headers: {'Content-Type': 'text/plain'},
-    body: 'johnny'.toUpperCase(),
+    headers: {
+      'Content-Type': 'text/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(pets),
   };
 };
 
